9f9205e867cccb5ead80781a21bce8dbaa976f9e
joc-gh <josh.cannon@gmail.com>
2022-01-21 14:48:44 +0000

    DrinksManager methods added

--- src/com/qa/divebar/Runner.java
+++ src/com/qa/divebar/Runner.java
@@ -1,11 +1,19 @@
 package com.qa.divebar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Runner {
+	private static List<Drink> drinks = new ArrayList<>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		JDBC_Setup jdbc_Setup = new JDBC_Setup();
 		jdbc_Setup.connect();
+	}
+
+	public static void addItem(Drink... i) {
+		drinks.addAll(Arrays.asList(i));
 	}
 
 }
