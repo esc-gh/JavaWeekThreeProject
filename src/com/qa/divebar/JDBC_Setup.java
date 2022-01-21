b6d1e28ffad14f7d25a31186fa09399b058e807d
joc-gh <josh.cannon@gmail.com>
2022-01-21 11:10:00 +0000

    JDBC setup and drinks manager

--- src/com/qa/divebar/JDBC_Setup.java
+++ src/com/qa/divebar/JDBC_Setup.java
@@ -2,12 +2,12 @@ package com.qa.divebar;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JDBC_Setup {
 	final String JDBC_Driver = "com.mysql.cj.jdbc.Driver";
 	final String db_url = "jdbc:mysql://localhost:3306/divebar";
-
 	final String user = "root";
 	final String password = "password here";
 
@@ -26,6 +26,21 @@ public class JDBC_Setup {
 		}
 
 		return conn;
+	}
+
+	public Drink returnResults(ResultSet resultSet) {
+		try {
+			int id = resultSet.getInt("id");
+			String name = resultSet.getString("name");
+			boolean alcholic = resultSet.getBoolean("alcholic");
+			float price = resultSet.getFloat("price");
+			String type = resultSet.getString("type");
+			Drink drink = new Drink(id, name, alcholic, price, type);
+			return drink;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
